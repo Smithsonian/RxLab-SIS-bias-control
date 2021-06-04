8277071a3ccbc36c632cfdb9c00f9f645da72b7c
John Garrett <john.garrett@astro.ox.ac.uk>
2021-06-04 09:35:38 -0400

    main: add VSET, VMON, IMON commands

--- main.py
+++ main.py
@@ -33,36 +33,58 @@ try:
         command[0] = command[0].upper()
 
         # Process command
-        # Set parameter
+        # If parameter, set parameter
         if command[0] in param.keys():
             param[command[0]] = float(command[1])
 
-        # Start voltage sweep
+        # SWEEP / START: Sweep control voltage
         elif command[0] == "SWEEP" or command[0] == "START":
-            print("")
             bias.sweep_control_voltage(vmin=param['VMIN'],
                                        vmax=param['VMAX'],
                                        period=param['PERIOD'],
                                        sample_rate=param['SAMPLE_RATE'])
+            # bias.start_iv_monitor_scan(period=param['PERIOD'],
+            #                            sample_rate=param['SAMPLE_RATE'])
 
+        # PULSE: Pulse control voltage
         elif command[0] == "PULSE":
             print("")
             bias.pulse_control_voltage(vmin=param['VMIN'],
                                        vmax=param['VMAX'],
                                        period=param['PERIOD'],
                                        sample_rate=param['SAMPLE_RATE'])
+            # bias.start_iv_monitor_scan(period=param['PERIOD'],
+            #                            sample_rate=param['SAMPLE_RATE'])
 
-        # Print info to terminal
+        # VSET: Set constant control voltage
+        elif command[0] == "VSET":
+            vctrl = float(command[1])
+            print("\n\tSet control voltage: {:.2f} V\n".format(vctrl))
+            bias.set_control_voltage(vctrl)
+
+        # VMON: Read voltage monitor
+        elif command[0] == "VMON":
+            vmon_mv = bias.read_voltage()
+            print("\n\tVoltage monitor: {:.2f} mV\n".format(vmon_mv))
+
+        # IMON: Read current monitor
+        elif command[0] == "IMON":
+            imon_ua = bias.read_current()
+            print("\n\tCurrent monitor: {:.1f} mA\n".format(imon_ua))
+
+        # INFO: Print info
         elif command[0] == "INFO":
+            print("")
             for key, value in param.items():
                 sep = "\t" if len(key) > 8 else "\t\t"
                 print(f"\t{key}{sep}{value}")
+            print("")
 
-        # Scan status
+        # STATUS: Print scan status
         elif command[0] == "STATUS":
-            print(f"Scanning: {bias.ao_scan_status() == 1}")
+            print(f"\n\tScanning: {bias.ao_scan_status() == 1}\n")
 
-        # Plot voltage and current
+        # PLOT: Plot I-V curve
         elif command[0] == "PLOT":
             if not bias.ao_scan_status():
                 print("You haven't started scanning yet...")
@@ -78,6 +100,9 @@ try:
             for i in range(npts):
                 voltage[i] = bias.read_voltage()
                 current[i] = bias.read_current()
+            # voltage, current
+            # data_in = list(self.analog_input)
+            # voltage, current = data_in[::2], data_in[1::2]
             ax.plot(voltage, current, 'ko', alpha=0.2, ms=1)
             plt.show()
 
@@ -98,39 +123,42 @@ try:
             #     fig.canvas.draw()
             #     plt.pause(0.1)
 
-        # Close all plots
+        # CLEAR: Clear all plots
         elif command[0] == "CLEAR":
             plt.close("all")
 
-        # Help
+        # HELP: Print help
         elif command[0] == "HELP" or command[0] == "H":
 
-            # Print commands
-            print("\nAvailable commands:")
-            print("\tHELP or H: Print help")
-            print("\tSWEEP or START: Sweep control voltage (triangle wave)")
-            print("\tPULSE: Pulse control voltage (square wave)")
-            print("\tINFO: Print all parameters")
-            print("\tSTATUS: Print scan status")
-            print("\tPLOT: Plot I-V curve")
-            print("\tCLEAR: Clear all plots")
-            print("\tSTOP or EXIT or Q: Close connection")
+            # Commands
+            print("\n\tAvailable commands:")
+            print("\t\tHELP or H: Print help")
+            print("\t\tSWEEP or START: Sweep control voltage (triangle wave)")
+            print("\t\tPULSE: Pulse control voltage (square wave)")
+            print("\t\tVSET: Set constant control voltage")
+            print("\t\tVMON: Read voltage monitor")
+            print("\t\tIMON: Read current monitor")
+            print("\t\tINFO: Print all parameters")
+            print("\t\tSTATUS: Print scan status")
+            print("\t\tPLOT: Plot I-V curve")
+            print("\t\tCLEAR: Clear all plots")
+            print("\t\tSTOP or EXIT or Q: Close connection")
 
-            # Print parameters
-            print("\nAvailable parameters:")
-            print("\tVMIN <value>: Minimum control voltage for sweep or pulse, in [V]")
-            print("\tVMAX <value>: Maximum control voltage for sweep or pulse, in [V]")
-            print("\tPERIOD <value>: Period of sweep or pulse, in [s]")
-            print("\tSAMPLE_RATE <value>: Sample rate for control voltage sweep or pulse, in [Hz]")
+            # Parameters
+            print("\n\tAvailable parameters:")
+            print("\t\tVMIN <value>: Minimum control voltage for sweep or pulse, in [V]")
+            print("\t\tVMAX <value>: Maximum control voltage for sweep or pulse, in [V]")
+            print("\t\tPERIOD <value>: Period of sweep or pulse, in [s]")
+            print("\t\tSAMPLE_RATE <value>: Sample rate for control voltage sweep or pulse, in [Hz]")
             print("")
 
-        # Stop
+        # STOP: Stop bias and shutdown
         elif command[0] == "STOP" or command[0] == "EXIT" or command[0] == "Q":
             break
 
-        # Not recognized
+        # Command not recognized
         else:
-            print("Command not recognized.")
+            print("\n\tCommand not recognized.\n")
 
 except KeyboardInterrupt:
     print("\nClosing program.")
