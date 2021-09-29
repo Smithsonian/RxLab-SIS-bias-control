b16153db42423024be5ad87e0824ae804d52797d
John Garrett <john.garrett@astro.ox.ac.uk>
2021-09-29 15:00:10 -0400

    Add utility module (util.py)

--- sisbias/__init__.py
+++ sisbias/__init__.py
@@ -1,3 +1,4 @@
 from .control import SISBias
+from .util import progress_bar, ask_filename
 
 __version__ = "0.0.1.dev"
---
+++ sisbias/util.py
@@ -0,0 +1,25 @@
+import os
+
+
+def progress_bar(iteration, total, prefix='', length=50):
+    percent = "{:.1f}".format(100 * (iteration / float(total)))
+    filledLength = int(length * iteration // total)
+    bar = "X" * filledLength + '-' * (length - filledLength)
+    print(f"\r{prefix} |{bar}| {percent}%", end="\r")
+    if iteration == total: 
+        print("")
+
+
+def ask_filename(msg="\tFile name: "):
+    print("")
+    while True:
+        fname = input(msg)
+        if fname == "":
+            return None
+        elif os.path.isfile(fname):
+            ans = input("\tFile already exists. Overwrite? [y/n] ")
+            if ans.lower() == "y":
+                return fname
+        else:
+            print("")
+            return fname
